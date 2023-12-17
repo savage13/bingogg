6d4d34f9cc3180eb2e31c348b69ba59ec6267a34
cjs8487 <cjschaefer1999@gmail.com>
2023-12-17 17:58:50 -0600

    cleanup room page

--- web/src/app/(main)/rooms/[slug]/page.tsx
+++ web/src/app/(main)/rooms/[slug]/page.tsx
@@ -5,85 +5,14 @@ import Board from '@/components/board/Board';
 import RoomLogin from '@/components/room/RoomLogin';
 import { ConnectionStatus, RoomContext } from '@/context/RoomContext';
 
-const board = [
-    [
-        'Collect 5 things',
-        'Talk to 10 people',
-        'A pointy device',
-        'Obtain a hammer',
-        'YELL',
-    ],
-    ['10 Wells', '15 flames', 'Beat the game', 'Die 10 times', 'Beat 5-2'],
-    [
-        'Beat 3-5',
-        'Discover Persia',
-        'Invent Religion',
-        'C O N Q U E S T',
-        'this is a really long goal name because sometimes these will exist and it needs to be tested against',
-    ],
-    [
-        'Return to Madagascar',
-        'Morph Ball',
-        '500 HP',
-        '50 coins',
-        'Have a civil war',
-    ],
-    ['Game over', 'Kill 300 enemies', 'Explosives', 'Compass', 'Roads'],
-];
-
-export default function Room({
-    params: { slug },
-}: {
-    params: { slug: string };
-}) {
-    const { connectionStatus } = useContext(RoomContext);
-    const [boardState, setBoardState] = useState([
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-    ]);
-    const [messages, setMessages] = useState<string[]>([]);
+export default function Room() {
+    const { connectionStatus, sendChatMessage, messages } =
+        useContext(RoomContext);
     const [message, setMessage] = useState('');
-
-    const { sendMessage, readyState, lastMessage, getWebSocket } = useWebSocket(
-        `ws://localhost:8000/rooms/${slug}`,
-    );
-
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessages((prev) => [...prev, lastMessage.data]);
-        }
-    }, [lastMessage, setMessages]);
 
     if (connectionStatus === ConnectionStatus.UNINITIALIZED) {
         return <RoomLogin />;
     }
-
-    const colors = [
-        'indigo',
-        'darkorange',
-        'maroon',
-        // 'purple',
-        // 'forestgreen',
-        // 'aqua',
-        // 'violet',
-        // 'red',
-        // 'white',
-        // 'lime',
-        // 'gray',
-        // 'yellow',
-        // '#2d587e',
-        // '#54f7e5',
-        // '#548715',
-        // '#58fe87',
-        // '#5e8a78',
-        // '#54a841',
-        // '#a7e452',
-        // '#157854',
-    ];
-    const colorPortion = 360 / colors.length;
 
     return (
         <div className="flex gap-x-8">
@@ -104,7 +33,7 @@ export default function Room({
                         onChange={(event) => setMessage(event.target.value)}
                         onKeyUp={(event) => {
                             if (event.key === 'Enter') {
-                                sendMessage(message);
+                                sendChatMessage(message);
                                 setMessage('');
                             }
                         }}
@@ -113,7 +42,7 @@ export default function Room({
                     <button
                         className="bg-gray-200 px-2 py-2"
                         onClick={() => {
-                            sendMessage(message);
+                            sendChatMessage(message);
                             setMessage('');
                         }}
                     >
