26a57ffb29e07a55c5a69f6ecc8a45e37ee3e70d
cjs8487 <cjschaefer1999@gmail.com>
2023-12-17 21:53:03 -0600

    remove unused file

--- web/src/hooks/RoomHooks.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Hook for connecting to and communicating with a room
- *
- * @param slug the room slug
- * @returns
- */
-export function useRoomConnection(slug: string) {
-    return {};
-}
