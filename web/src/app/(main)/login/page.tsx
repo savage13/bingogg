1de159c664c13180394b1ae7b063f1e7bcb0df68
cjs8487 <cjschaefer1999@gmail.com>
2024-01-04 18:38:04 -0600

    login form adjustments

--- web/src/app/(main)/login/page.tsx
+++ web/src/app/(main)/login/page.tsx
@@ -5,52 +5,53 @@ import Link from 'next/link';
 export default function Login() {
     return (
         <div className="flex h-full items-center justify-center">
-            <Formik
-                initialValues={{ username: '', password: '' }}
-                onSubmit={function ({
-                    username,
-                    password,
-                }: FormikValues): void | Promise<any> {}}
-            >
-                <Form className="flex max-w-[50%] flex-col items-center rounded-3xl border-4 px-8 py-6">
-                    <div className="pb-1 text-3xl font-bold">
-                        Login to bingo.gg
-                    </div>
-                    <div className="pb-8 text-justify text-sm text-gray-300">
-                        You only need to login if you plan to manage a game. No
-                        login is required to play bingo.
-                    </div>
-                    <label className="pb-4">
-                        <span className="pr-3">Username</span>
-                        <Field name="username" className="text-black" />
-                    </label>
-                    <label>
-                        <span className="pr-3">Password</span>
-                        <Field
-                            type="password"
-                            name="password"
-                            className="text-black"
-                        />
-                    </label>
-                    <Link href="" className="pl-4 pt-0.5 text-sm underline">
-                        Forgot password?
-                    </Link>
-                    <div className="w-full pt-4">
-                        <Link
-                            href="/register"
-                            className="float-left    rounded-md border px-4 py-2 shadow-sm shadow-white"
-                        >
-                            Register
-                        </Link>
-                        <button
-                            type="submit"
-                            className="float-right rounded-md border px-4 py-2 shadow-sm shadow-white"
-                        >
-                            Log In
-                        </button>
-                    </div>
-                </Form>
-            </Formik>
+            <div className="flex max-w-[50%] grow flex-col items-center rounded-3xl border-4 px-8 py-6">
+                <div className="pb-1 text-3xl font-bold">Login to bingo.gg</div>
+                <div className="pb-4 text-justify text-sm text-gray-300">
+                    No login is required to play bingo.
+                </div>
+                <Formik
+                    initialValues={{ username: '', password: '' }}
+                    onSubmit={({ username, password }) => {}}
+                >
+                    <Form className="flex w-full flex-col items-center justify-center gap-y-3">
+                        <label className="w-3/4">
+                            <div>Username</div>
+                            <Field
+                                name="username"
+                                className="w-full text-black"
+                            />
+                        </label>
+                        <div className="w-3/4">
+                            <label>
+                                <div className="">Password</div>
+                                <Field
+                                    type="password"
+                                    name="password"
+                                    className="w-full text-black"
+                                />
+                            </label>
+                            <Link href="" className="pt-0.5 text-sm underline">
+                                Forgot password?
+                            </Link>
+                        </div>
+                        <div className="w-full pt-1">
+                            <Link
+                                href="/register"
+                                className="float-left rounded-md border px-4 py-2 shadow-sm shadow-white hover:bg-gray-800"
+                            >
+                                Register
+                            </Link>
+                            <button
+                                type="submit"
+                                className="float-right rounded-md border px-4 py-2 shadow-sm shadow-white hover:bg-gray-800"
+                            >
+                                Log In
+                            </button>
+                        </div>
+                    </Form>
+                </Formik>
+            </div>
         </div>
     );
 }
