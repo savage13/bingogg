7904cd26267500270234707db0212f9bfb9f4ed1
cjs8487 <cjschaefer1999@gmail.com>
2024-01-06 15:10:40 -0600

    fix proxy issue with query parameters

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -2,8 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+    console.log(request.nextUrl);
     return NextResponse.rewrite(
-        `${process.env.API_PATH}/${request.nextUrl.pathname}`,
+        `${process.env.API_PATH}/${request.nextUrl.pathname}${
+            request.nextUrl.search ? `?${request.nextUrl.search}` : ''
+        }`,
     );
 }
 
