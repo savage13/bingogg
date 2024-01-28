c52d159c8a9cedaf46431f0104c9a4d37ad22ea2
cjs8487 <cjschaefer1999@gmail.com>
2024-01-28 01:05:12 -0600

    add cookie banner

--- web/src/app/(main)/layout.tsx
+++ web/src/app/(main)/layout.tsx
@@ -1,10 +1,24 @@
+'use client';
 import { ReactNode } from 'react';
 import Header from '../../components/header/Header';
+import CookieConsent from 'react-cookie-consent';
 
 export default function CoreLayout({ children }: { children: ReactNode }) {
     return (
         <div className="h-screen bg-background text-text">
             <Header />
+            <CookieConsent
+                location="bottom"
+                buttonText="I understand"
+                buttonStyle={{
+                    background: '#8c091b',
+                    color: '#fbfbfb',
+                    fontSize: '13px',
+                }}
+            >
+                This website uses cookies to provide some parts of it&#39;s
+                functionality.
+            </CookieConsent>
             <div className="h-[calc(100%-60px)] p-4">{children}</div>
         </div>
     );
