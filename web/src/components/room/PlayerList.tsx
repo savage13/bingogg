538e0e9b8c537ea0e9423745ccfde57c2e5d5ef6
cjs8487 <cjschaefer1999@gmail.com>
2024-05-12 15:39:16 -0500

    start setting up player list for racetime

--- web/src/components/room/PlayerList.tsx
+++ web/src/components/room/PlayerList.tsx
@@ -2,20 +2,29 @@ import { useContext } from 'react';
 import { RoomContext } from '../../context/RoomContext';
 
 export default function PlayerList() {
-    const { players } = useContext(RoomContext);
+    const { players, roomData } = useContext(RoomContext);
+    const racetimeConnected = !!roomData?.racetimeUrl;
+
     return (
         <div className="block max-h-full overflow-y-auto rounded-md border border-border bg-foreground px-3 py-2">
             <div className="pb-1 text-lg font-semibold">Connected Players</div>
             <div className="flex flex-col gap-y-3">
                 {players.map((player) => (
-                    <div key={player.nickname} className="flex gap-x-2">
-                        <div
-                            className="rounded-md border px-1"
-                            style={{ background: player.color }}
-                        >
-                            {player.goalCount}
+                    <div key={player.nickname}>
+                        <div className="flex gap-x-2">
+                            <div
+                                className="rounded-md border px-1"
+                                style={{ background: player.color }}
+                            >
+                                {player.goalCount}
+                            </div>
+                            <div>{player.nickname}</div>
                         </div>
-                        <div>{player.nickname}</div>
+                        {racetimeConnected && (
+                            <div className="pl-1 pt-1.5 text-sm">
+                                Not connected
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
