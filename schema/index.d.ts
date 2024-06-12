ffabf3337fdca653aeb615fa851aa71accf5bfd7
cjs8487 <cjschaefer1999@gmail.com>
2024-06-11 21:07:52 -0500

    [types] setup for publishing

---
+++ schema/index.d.ts
@@ -0,0 +1,19 @@
+export { Board } from './types/Board';
+export { Cell } from './types/Cell';
+export { MessageSegment, ChatMessage } from './types/ChatMessage';
+export { Game } from './types/Game';
+export { Goal } from './types/Goal';
+export { Player } from './types/Player';
+export {
+    RoomAction,
+    JoinAction,
+    LeaveAction,
+    ChatAction,
+    MarkAction,
+    UnmarkAction,
+    ChangeColorAction,
+    NewCardAction,
+} from './types/RoomAction';
+export { RoomData } from './types/RoomData';
+export { ServerMessage } from './types/ServerMessage';
+export { User } from './types/User';
